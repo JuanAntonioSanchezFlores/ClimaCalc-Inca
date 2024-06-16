3f7bd3a2bd201d79a25aa2232dba9e4fd41d585c
JuanAntonioSanchezFlores <115922578+JuanAntonioSanchezFlores@users.noreply.github.com>
2024-06-16 20:42:27 +0200

    Eliminar credenciales  environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig:{
-    apiKey: "AIzaSyA7JfKfTTJfXR6r5f1p7WG0v14YJFEfBgM",
-    authDomain: "appclimatizacion.firebaseapp.com",
-    projectId: "appclimatizacion",
-    storageBucket: "appclimatizacion.appspot.com",
-    messagingSenderId: "523875685759",
-    appId: "1:523875685759:web:6232805ff008c48540f64a"
+   
   }
 };
 
